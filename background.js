68f1a9aaeb027e1ffc5c3301377011279ae6f329
Petli1 <84804756+Petli1@users.noreply.github.com>
2022-01-27 15:26:34 +0000

    button2.1

--- background.js
+++ background.js
@@ -1,4 +1,10 @@
 var enabled = true;
+const click = (clazz) => {
+    const buttons = document.getElementsByClassName(clazz);
+    for (const button of buttons) {
+      button.click();
+    }
+}
 chrome.webRequest.onBeforeRequest.addListener(
   function(info) {
     if(!enabled)                                           // if the extension is not enabled
--- skipper.js
+++ skipper.js
@@ -1,9 +1,3 @@
-const click = (clazz) => {
-  const buttons = document.getElementsByClassName(clazz);
-  for (const button of buttons) {
-    button.click();
-  }
-}
 if (document.querySelector("div.ad-showing")) {
   document.getElementsByTagName("video")[0].playbackRate = 16;
 }
