bb537cfc9533ac93b5336c0bb7cc152cde774fd9
Petli1 <84804756+Petli1@users.noreply.github.com>
2022-01-27 17:06:59 +0000

    nothing works

--- background.js
+++ background.js
@@ -1,20 +1,15 @@
-var enabled = true;
+var enabled;
 const click = (clazz) => {
     const buttons = document.getElementsByClassName(clazz);
     for (const button of buttons) {
       button.click();
     }
 }
-chrome.webRequest.onBeforeRequest.addListener(
-  function(info) {
-    if(!enabled) {                                         // if the extension is not enabled
-      return { cancel: false };                            // don't cancel or redirect
-    }
-    else{
+chrome.browserAction.onClicked.addListener(function (tab) {
+    if (enabled==true){
         setInterval(() => {
             click("ytp-ad-skip-button-text");
             click("ytp-ad-overlay-close-button");
         }, 300)
     }
-    }
-);
+});
