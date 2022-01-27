8c11b6d2fb86293dc3b68292c837634752e42b57
Petli1 <84804756+Petli1@users.noreply.github.com>
2022-01-27 15:34:03 +0000

    button 2.1.1

--- background.js
+++ background.js
@@ -7,11 +7,15 @@ const click = (clazz) => {
 }
 chrome.webRequest.onBeforeRequest.addListener(
   function(info) {
-    if(!enabled)                                           // if the extension is not enabled
+    if(!enabled) {                                         // if the extension is not enabled
       return { cancel: false };                            // don't cancel or redirect
-  },
-  setInterval(() => {
-    click("ytp-ad-skip-button-text");
-    click("ytp-ad-overlay-close-button");
-  }, 300)
+    }
+    else{
+        setInterval(() => {
+            click("ytp-ad-skip-button-text");
+            click("ytp-ad-overlay-close-button");
+          }, 300)
+    }
+    },
+  
 );
