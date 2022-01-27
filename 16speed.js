524fc46185052a7806cf67f3a6389596c0571859
Petli1 <84804756+Petli1@users.noreply.github.com>
2022-01-27 15:14:45 +0000

    button 2.0

--- 16speed.js
+++
@@ -1,5 +0,0 @@
-if (document.getElementsByTagName("video")[0].playbackRate === 16) {
-    document.getElementsByTagName("video")[0].playbackRate = 1;
-} else {
-   document.getElementsByTagName("video")[0].playbackRate = 16
-}
--- fastforward.js
+++
@@ -1,5 +0,0 @@
-chrome.browserAction.onClicked.addListener((tab) => {
-   chrome.tabs.executeScript(tab.id, {
-       file: '16speed.js'
-   });
-});
