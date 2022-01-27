b542078098a2ed4632446cbf8486c2c58c5e6dfc
Petli1 <84804756+Petli1@users.noreply.github.com>
2022-01-27 14:39:58 +0000

    Update skipper.js

--- skipper.js
+++ skipper.js
@@ -1,8 +1,10 @@
 const click = (clazz) => {
-  const buttons = document.getElementsByClassName(clazz);
-  for (const button of buttons) {
-    button.click();
+    const buttons = document.getElementsByClassName(clazz);
+    for (const button of buttons) {
+      button.click();
+    }
   }
+<<<<<<< HEAD
 }
 if (document.querySelector("div.ad-showing")) {
   document.getElementsByTagName("video")[0].playbackRate = 16
@@ -11,3 +13,10 @@ if (document.querySelector("div.ad-showing")) {
     click("ytp-ad-overlay-close-button");
   }, 300);
 }
+=======
+  
+  setInterval(() => {
+    click("ytp-ad-skip-button-text");
+    click("ytp-ad-overlay-close-button");
+  }, 300);
+>>>>>>> parent of 4aa051f (auto fast forward)
