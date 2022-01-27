fcfb3f850b3e2146d43b8fe4307892a8950ebf98
Petli1 <84804756+Petli1@users.noreply.github.com>
2022-01-27 14:59:02 +0000

    with skipper toggle

--- skipper.js
+++ skipper.js
@@ -11,7 +11,14 @@ if (document.querySelector("div.ad-showing")) {
     click("ytp-ad-overlay-close-button");
   }, 300);
 }
-setInterval(() => {
-  click("ytp-ad-skip-button-text");
-  click("ytp-ad-overlay-close-button");
-}, 300);
+var enabled = true;
+chrome.webRequest.onBeforeRequest.addListener(
+  function(info) {
+    if(!enabled)                                           // if the extension is not enabled
+      return { cancel: false };                            // don't cancel or redirect
+  },
+  setInterval(() => {
+    click("ytp-ad-skip-button-text");
+    click("ytp-ad-overlay-close-button");
+  }, 300)
+);
