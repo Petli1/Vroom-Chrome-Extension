83c02ac63d1b55551ba9ede0afdc402bf2d7fd3f
Petli1 <84804756+Petli1@users.noreply.github.com>
2022-01-27 13:28:29 +0000

    Add files via upload

---
+++ Vroom Chrome Extension/16speed.js
@@ -0,0 +1,5 @@
+if (document.getElementsByTagName("video")[0].playbackRate === 16) {
+    document.getElementsByTagName("video")[0].playbackRate = 1;
+} else {
+   document.getElementsByTagName("video")[0].playbackRate = 16
+}
