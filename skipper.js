4aa051fc14b5b3ed8d4f6a906f8a003287c16699
Petli1 <84804756+Petli1@users.noreply.github.com>
2022-01-27 14:30:19 +0000

    auto fast forward

--- skipper.js
+++ skipper.js
@@ -1,11 +1,13 @@
 const click = (clazz) => {
-    const buttons = document.getElementsByClassName(clazz);
-    for (const button of buttons) {
-      button.click();
-    }
+  const buttons = document.getElementsByClassName(clazz);
+  for (const button of buttons) {
+    button.click();
   }
-  
-  setInterval(() => {
-    click("ytp-ad-skip-button-text");
-    click("ytp-ad-overlay-close-button");
-  }, 300);
+}
+setInterval(() => {
+  click("ytp-ad-skip-button-text");
+  click("ytp-ad-overlay-close-button");
+}, 300);
+if (document.querySelector("div.ad-showing")) {
+  document.getElementsByTagName("video")[0].playbackRate = 16
+}
